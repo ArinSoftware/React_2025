cb87427698652118162487bb56150006725ce769
gurcancekic <arinyazilim@gmail.com>
2024-12-15 13:47:32 +0300

    Lesson 05

---
+++ about-react/src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.querySelector('#root');
+
+function App() {
+    return <h1>Hello React</h1>;
+}
+
+createRoot(rootElement).render(
+    <App />
+);
