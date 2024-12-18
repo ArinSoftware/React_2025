3988af180038afa36e2126cc64c7176269272833
gurcancekic <arinyazilim@gmail.com>
2024-12-18 22:53:10 +0300

    Lesson 07

--- about-react/src/main.jsx
+++ about-react/src/main.jsx
@@ -1,29 +1,31 @@
 import { createRoot } from "react-dom/client";
 
+import "./App.css";
+
 const rootElement = document.querySelector('#root');
 
 function App() {
     return (
-        <div>
+        <div className="content">
             <Header />
             <TeamMembers />
         </div>
-
     )
 }
 
+
 function Header() {
     return (
-        <div class="title">
-            <h1>Team Members</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-        </div>
+		<div className="title">
+			<h1>Team Members</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</div>
     ) 		
 }
 
 function TeamMembers() {
     return (
-        <ul class="team">
+        <ul className="team">
             <TeamMember />
             <TeamMember />
             <TeamMember />
@@ -34,9 +36,9 @@ function TeamMembers() {
 
 function TeamMember() {
     return (
-        <li class="member co-funder">
-            <div class="thumb"><img src="https://assets.codepen.io/3/internal/avatars/users/default.png?fit=crop&format=auto&height=120&width=120" /></div>
-            <div class="description">
+        <li className="member co-funder">
+            <div className="thumb"><img src="https://assets.codepen.io/3/internal/avatars/users/default.png?fit=crop&format=auto&height=120&width=120" /></div>
+            <div className="description">
                 <h3>Chris Coyier</h3>
                 <p>Chris is a front-end developer and designer. He writes a bunch of HTML, CSS, and JavaScript and shakes the pom-poms for CodePen.<br/><a href="https://codepen.io/chriscoyier/">@chriscoyier</a></p>
             </div>
