87ede803c0ab83630654ac7d1a0e9afd6bc44952
gurcancekic <arinyazilim@gmail.com>
2024-12-24 20:33:47 +0300

    Lesson 10

---
+++ employees-react/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+
+  let count = 0;
+
+  function handleIncrement() {
+    console.log("handleIncrement FIRED");
+    count += 1;
+    console.log("count", count);
+  }
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
