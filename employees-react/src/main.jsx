b8e9ebb34fbcc76385d81df238b6c19293b1c3db
gurcancekic <arinyazilim@gmail.com>
2024-12-25 21:18:31 +0300

    Lesson 11

--- employees-react/src/main.jsx
+++ employees-react/src/main.jsx
@@ -1,24 +1,96 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import './index.css'
 
 function App() {
 
-  let count = 0;
-
-  function handleIncrement() {
-    console.log("handleIncrement FIRED");
-    count += 1;
-    console.log("count", count);
-  }
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "David Hardy",
+      email: "thomashardy@mail.com",
+      address: "89 Chiaroscuro Rd, Portland, USA",
+      phone: "(171) 555-2222"
+    }
+  ]);
 
   return (
-    <div>
-      <h2>Count: {count}</h2>
-      <div>
-        <button onClick={handleIncrement}>Increment</button>
-        <button>Decrement</button>
+    <div className="container">
+      <div className="table-wrapper">
+        <Header />
+        <EmployeeList employees={employees}/>
       </div>
     </div>
+  )
+
+}
+
+function Header() {
+  return (
+    <div className="table-title">
+      <div className="row">
+        <div className="col-sm-6">
+          <h2>Manage <b>Employees</b></h2>
+        </div>
+        <div className="col-sm-6">
+          <a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+          <a href="#deleteEmployeeModal" className="btn btn-danger" data-toggle="modal"><i className="material-icons">&#xE15C;</i> <span>Delete</span></a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function EmployeeList({employees}) {
+  console.log("emp", employees);
+  return (
+    <table className="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>
+            <span className="custom-checkbox">
+              <input type="checkbox" id="selectAll" />
+              <label htmlFor="selectAll"></label>
+            </span>
+          </th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          employees.map( employee => (
+            <EmployeeItem key={employee.id} employee={employee} />
+          ))
+        }
+      </tbody>
+    </table>
+  )
+}
+
+function EmployeeItem( {employee}) {
+  console.log("employee", employee);
+  return (
+    <tr>
+      <td>
+        <span className="custom-checkbox">
+          <input type="checkbox" id="checkbox1" name="options[]" value="1" />
+          <label htmlFor="checkbox1"></label>
+        </span>
+      </td>
+      <td>{employee.name}</td>
+      <td>{employee.email}</td>
+      <td>{employee.address}A</td>
+      <td>{employee.phone}</td>
+      <td>
+        <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+        <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+      </td>
+    </tr>
   )
 }
 
