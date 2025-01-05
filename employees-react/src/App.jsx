daf1ccac1c71c674132205dcfe10b61957e370a9
gurcancekic <arinyazilim@gmail.com>
2025-01-05 14:56:07 +0300

    Lesson 16

--- employees-react/src/App.jsx
+++ employees-react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import AddEmployeeModal from "./components/AddEmployeeModal";
+import EditEmployeeModal from "./components/EditModalEmployee";
 import EmployeeList from "./components/EmployeeList";
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
             phone: "(171) 555-2222"
         }
     ]);
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
 
     function addEmployee(newEmployee) {
         setEmployees(prevEmployees => [
@@ -26,14 +30,27 @@ function App() {
         ])
     }
 
-    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    function editClick(employee) {
+        setIsEditModalOpen(true);
+        setSelectedEmployee(employee)
+    }
+
+    
 
     return (
         <div className="container">
             <div className="table-wrapper">
                 <Header onOpenAddModal={() => setIsAddModalOpen(true)} />
-                <EmployeeList employees={employees} />
+                <EmployeeList employees={employees} onEditClick={editClick} />
                 <AddEmployeeModal isOpen={isAddModalOpen} onCloseAddModal={() => setIsAddModalOpen(false)} onAddEmployee={addEmployee} />
+                <EditEmployeeModal 
+                    isOpen={isEditModalOpen} 
+                    employee={selectedEmployee} 
+                    onCloseEditModal={ () => {
+                        setIsEditModalOpen(false);
+                        setSelectedEmployee(null);
+                    }}
+                    />
             </div>
         </div>
     )
