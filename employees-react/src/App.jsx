83c9b5569a279da5c5a346a39ca1d2d04bef81ac
gurcancekic <arinyazilim@gmail.com>
2025-01-12 15:53:38 +0300

    Lesson 19

--- employees-react/src/App.jsx
+++ employees-react/src/App.jsx
@@ -14,11 +14,19 @@ function App() {
             email: "thomashardy@mail.com",
             address: "89 Chiaroscuro Rd, Portland, USA",
             phone: "(171) 555-2222"
+        },
+        {
+            id: 2,
+            name: "Ricardo Quaresma",
+            email: "ricody@mail.com",
+            address: "89 Chiaroscuro Rd, Portland, USA",
+            phone: "(171) 444-5555"
         }
     ]);
     const [isAddModalOpen, setIsAddModalOpen] = useState(false);
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [selectedEmployee, setSelectedEmployee] = useState(null);
+    const [selectedEmployees, setSelectedEmployees] = useState([]);
 
     function addEmployee(newEmployee) {
         setEmployees(prevEmployees => [
@@ -31,11 +39,8 @@ function App() {
     }
 
     function editEmployee(updatedEmployee) {
-
-        console.log("updatedEmployee", updatedEmployee);
-
-        setEmployees(prevEmployees => 
-            prevEmployees.map(emp => 
+        setEmployees(prevEmployees =>
+            prevEmployees.map(emp =>
                 emp.id === updatedEmployee.id ? updatedEmployee : emp
             )
         )
@@ -46,18 +51,46 @@ function App() {
         setSelectedEmployee(employee)
     }
 
+    function deleteClick(employee) {
+        const confirmed = window.confirm("Are you sure you want to deletre this employee?");
+
+        if (confirmed) {
+            setEmployees(prevEmployees =>
+                prevEmployees.filter(emp => emp.id !== employee.id)
+            );
+        }
+    }
+
     function closeEditModal() {
         setIsEditModalOpen(false);
         setSelectedEmployee(null);
     }
 
+    function deleteSelectedEmployees() {
+        const confirmed = window.confirm("Are you sure you want to delete the employees?");
+
+        if (confirmed) {
+            setEmployees(prevEmployees =>
+                prevEmployees.filter(emp => !selectedEmployees.includes(emp.id))
+            )
+        }
+    }
 
 
     return (
         <div className="container">
             <div className="table-wrapper">
-                <Header onOpenAddModal={() => setIsAddModalOpen(true)} />
-                <EmployeeList employees={employees} onEditClick={editClick} />
+                <Header
+                    onOpenAddModal={() => setIsAddModalOpen(true)}
+                    onDeleteSelected={deleteSelectedEmployees}
+                />
+                <EmployeeList
+                    employees={employees}
+                    onEditClick={editClick}
+                    onDeleteClick={deleteClick}
+                    selectedEmployees = {selectedEmployees}
+                    setSelectedEmployees = {setSelectedEmployees}
+                />
                 <AddEmployeeModal isOpen={isAddModalOpen} onCloseAddModal={() => setIsAddModalOpen(false)} onAddEmployee={addEmployee} />
                 <EditEmployeeModal
                     isOpen={isEditModalOpen}
